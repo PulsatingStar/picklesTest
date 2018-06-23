63ef47e40b485033d5638325035c4928302c24b8
PulsatingStar <vishal.kalsi22@gmail.com>
2018-06-23 14:49:56 -0400

    Pickles code
    Added Pickles Code

---
+++ pickledFeatures.js
@@ -0,0 +1,105 @@
+jsonPWrapper ({
+  "Features": [
+    {
+      "RelativeFolder": "emails.feature",
+      "Feature": {
+        "Name": "Title of your feature",
+        "Description": "I want to use this template for my feature file",
+        "FeatureElements": [
+          {
+            "Name": "Title of your scenario",
+            "Slug": "title-of-your-scenario",
+            "Description": "",
+            "Steps": [
+              {
+                "Keyword": "Given",
+                "NativeKeyword": "Given ",
+                "Name": "I want to write a step with precondition",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "When",
+                "NativeKeyword": "When ",
+                "Name": "I complete action",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              },
+              {
+                "Keyword": "Then",
+                "NativeKeyword": "Then ",
+                "Name": "I validate the outcomes",
+                "StepComments": [],
+                "AfterLastStepComments": []
+              }
+            ],
+            "Tags": [
+              "@Awesome"
+            ],
+            "Result": {
+              "WasExecuted": false,
+              "WasSuccessful": false
+            }
+          }
+        ],
+        "Result": {
+          "WasExecuted": false,
+          "WasSuccessful": false
+        },
+        "Tags": [
+          "@Awesome"
+        ]
+      },
+      "Result": {
+        "WasExecuted": false,
+        "WasSuccessful": false
+      }
+    }
+  ],
+  "Summary": {
+    "Tags": [
+      {
+        "Tag": "@Awesome",
+        "Total": 1,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 1
+      }
+    ],
+    "Folders": [
+      {
+        "Folder": "emails.feature",
+        "Total": 1,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 1
+      }
+    ],
+    "NotTestedFolders": [
+      {
+        "Folder": "emails.feature",
+        "Total": 0,
+        "Passing": 0,
+        "Failing": 0,
+        "Inconclusive": 0
+      }
+    ],
+    "Scenarios": {
+      "Total": 1,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 1
+    },
+    "Features": {
+      "Total": 1,
+      "Passing": 0,
+      "Failing": 0,
+      "Inconclusive": 1
+    }
+  },
+  "Configuration": {
+    "SutName": "Test",
+    "SutVersion": "V1.1",
+    "GeneratedOn": "23 June 2018 14:41:11"
+  }
+});
